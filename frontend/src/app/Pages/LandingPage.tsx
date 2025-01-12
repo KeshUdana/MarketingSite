6416cadd5f1c988d02e76db1603545f01f60557b
nelith-2002 <nelith.20230664@iit.ac.lk>
2025-01-12 10:40:18 +0530

    create LnadingPage

---
+++ frontend/src/app/Pages/LandingPage.tsx
@@ -0,0 +1,111 @@
+import { FunctionComponent } from 'react';
+import styles from './LandingPage.module.css';
+
+
+const LandingPage:FunctionComponent = () => {
+  	return (
+    		<div className={styles.landingPage}>
+      			<img className={styles.landingPageChild} alt="" src="Vector 10.svg" />
+      			<img className={styles.landingPageItem} alt="" src="Ellipse 13.svg" />
+      			<img className={styles.landingPageInner} alt="" src="Vector 6.svg" />
+      			<div className={styles.rectangleDiv} />
+      			<img className={styles.vectorIcon} alt="" src="Vector 5.svg" />
+      			<img className={styles.landingPageChild1} alt="" src="Vector 4.svg" />
+      			<div className={styles.homeAboutUs}>Home          About Us         Login        Contact Us</div>
+      			<img className={styles.component1Icon} alt="" src="Component 1.png" />
+      			<img className={styles.groupIcon} alt="" src="Group 5.png" />
+      			<img className={styles.landingPageChild2} alt="" src="Group 6.png" />
+      			<div className={styles.weHelpYouContainer}>
+        				<p className={styles.weHelpYouToGrowYour}>
+          					<span className={styles.weHelpYou}>
+            						<span className={styles.weHelpYou1}>We Help you</span>
+            						<span className={styles.span}>{` `}</span>
+          					</span>
+          					<span className={styles.toGrowYour}>to grow your</span>
+        				</p>
+        				<p className={styles.business}>Business</p>
+      			</div>
+      			<img className={styles.ellipseIcon} alt="" src="Ellipse 8.svg" />
+      			<img className={styles.landingPageChild3} alt="" src="Ellipse 9.svg" />
+      			<img className={styles.landingPageChild4} alt="" src="Ellipse 10.svg" />
+      			<img className={styles.icon} alt="" src="12967496761553233092 (1) 2.svg" />
+      			<img className={styles.icon1} alt="" src="14459920111530103316 2.svg" />
+      			<img className={styles.icon2} alt="" src="16304616191553750378 2.svg" />
+      			<div className={styles.whatWeDoContainer}>
+        				<p className={styles.whatWeDo}>What We Do</p>
+        				<p className={styles.weHelpYouToGrowYour}>For Your Business</p>
+      			</div>
+      			<div className={styles.aboutUsWhatContainer}>
+        				<p className={styles.whatWeDo}>About Us</p>
+        				<p className={styles.weHelpYouToGrowYour}>What is Modde?</p>
+          					</div>
+          					<div className={styles.landingPageChild5} />
+          					<div className={styles.landingPageChild6} />
+          					<div className={styles.landingPageChild7} />
+          					<div className={styles.landingPageChild8} />
+          					<div className={styles.enhanceCustomerReach}>Enhance Customer Reach</div>
+          					<div className={styles.connectYourProducts}>Connect your products with a wider audience through AI-driven recommendations.</div>
+          					<div className={styles.landingPageChild9} />
+          					<img className={styles.website49431Icon} alt="" src="website-4943 1.svg" />
+          					<div className={styles.streamlineProductDiscovery}>Streamline Product Discovery</div>
+          					<div className={styles.helpCustomersFind}>Help customers find similar products effortlessly by uploading an image.</div>
+          					<div className={styles.landingPageChild10} />
+          					<div className={styles.boostLocalSales}>Boost Local Sales</div>
+          					<div className={styles.romoteSriLankan}>romote Sri Lankan retailers and increase visibility within the local market.</div>
+          					<div className={styles.landingPageChild11} />
+          					<div className={styles.improveShoppingExperience}>Improve Shopping Experience</div>
+          					<div className={styles.deliverAPersonalized}>Deliver a personalized and intuitive shopping journey for your customers.</div>
+          					<div className={styles.landingPageChild12} />
+          					<img className={styles.mobilePhone11642Icon} alt="" src="mobile-phone-1164 2.svg" />
+          					<img className={styles.mobilePhone11641Icon} alt="" src="mobile-phone-1164 1.svg" />
+          					<img className={styles.rocket34291Icon} alt="" src="rocket-3429 1.svg" />
+          					<img className={styles.landingPageChild13} alt="" src="Group 1.svg" />
+          					<div className={styles.loremIpsumHas}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+            						<div className={styles.loremIpsumHas1}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+              							<div className={styles.loremIpsumHas2}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                								<div className={styles.button}>
+                  									<div className={styles.buttonChild} />
+                  									<b className={styles.getStarted}>Get Started</b>
+                  									<div className={styles.buttonItem} />
+                  									<div className={styles.getStarted1}>GET STARTED</div>
+                								</div>
+                								<div className={styles.landingPageChild14} />
+                								<img className={styles.landingPageChild15} alt="" src="Vector 13.svg" />
+                								<div className={styles.vectorParent}>
+                  									<img className={styles.groupChild} alt="" src="Vector 12.svg" />
+                  									<div className={styles.ourTestimonialsWhatContainer}>
+                    										<p className={styles.whatWeDo}>Our Testimonials</p>
+                    										<p className={styles.weHelpYouToGrowYour}>What Our Clients Are Saying</p>
+                  									</div>
+                  									<div className={styles.groupItem} />
+                  									<img className={styles.icon3} alt="" src="19399522241579780463 1.svg" />
+                  									<div className={styles.loremIpsumHas3}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                    										<div className={styles.name}>Name</div>
+                    										<div className={styles.groupInner} />
+                    										<img className={styles.icon4} alt="" src="19399522241579780463 2.svg" />
+                    										<div className={styles.loremIpsumHas4}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                      											<img className={styles.groupChild1} alt="" src="Vector 14.svg" />
+                      											<div className={styles.name1}>Name</div>
+                    										</div>
+                    										<div className={styles.landingPageChild16} />
+                    										<div className={styles.loremIpsumHas5}>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</div>
+                      											<div className={styles.landingPageChild17} />
+                      											<div className={styles.landingPageChild18} />
+                      											<div className={styles.getInTouch}>GET IN TOUCH</div>
+                      											<div className={styles.enterYourEmail}>Enter Your Email</div>
+                      											<div className={styles.send}>{`send `}</div>
+                      											<img className={styles.component2Icon} alt="" src="Component 2.png" />
+                      											<div className={styles.button1}>
+                        												<div className={styles.buttonInner} />
+                        												<b className={styles.getStarted}>Get Started</b>
+                        												<div className={styles.buttonItem} />
+                        												<div className={styles.getStarted1}>EXPLORE MORE</div>
+                      											</div>
+                      											<div className={styles.frameDiv}>
+                        												<div className={styles.frameChild} />
+                      											</div>
+                    										</div>);
+                    										};
+                    										
+                    										export default LandingPage;
+                    										
