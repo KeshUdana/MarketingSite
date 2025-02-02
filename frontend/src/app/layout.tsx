8501ed7057a0b260fa3f4b46b19d127e34df7d0f
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2025-02-02 23:22:55 +0530

    google verification

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Lato } from 'next/font/google';
 import './globals.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Head from 'next/head';  // Import Head from next/head
 
 const lato = Lato({
   subsets: ['latin'],
@@ -22,6 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={lato.className}>
+      <Head>
+        {/* Google site verification meta tag */}
+        <meta name="google-site-verification" content="IPL0B40qZB1CXwzNooV_4gd-VuPkKq2UW2Oh9abwVwQ" />
+      </Head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
